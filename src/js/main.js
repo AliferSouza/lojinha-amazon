b1d8403d49bb0e60043091eb58b1c14d7a12e8bd
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2021-07-24 23:38:13 -0300

    Mais um commit link

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,33 @@
 
+
+
+
 fetch("dados.json")
     .then(response => response.json())
     .then(data => {
-      
+        console.log(data)
+        show(data);
     }).catch(e => {
-        console.log(e)
+        console.log("ERRO" + e)
     })
 
 
+function show(posts) {
+    let output = ''
+
+    for (let post of posts) {
+
+        output += `<div class="card">
+            <img src="${post.src}" width="70" height="70">
+            <h2>${post.Titulo}</h2>
+            <br>
+            <br>
+            <a class="btn" href="${post.Link}">Adquerir</a>
+        </div>
+`
+    }
+    document.querySelector('.conteiner-cards').innerHTML = output
+}
 
 
 
